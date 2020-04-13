b98e38df5da4396cbe5a1e4838a2e9111bed431e
yong <den9yon9@gmail.com>
2020-04-13 10:54:43 +0800

    fix: auto.sh

--- auto.sh
+++ auto.sh
@@ -5,10 +5,10 @@ git clone https://github.com/den9yon9/config-backup.git
 cp ./config-backup/.gitconfig .
 
 # 安装并配置v2ray
-chmod a+x ./v2ray-install.sh && ./v2ray-install.sh -y && cp ./config-backup/config.json /etc/v2ray/
+chmod a+x ./config-backup/v2ray-install.sh && ./config-backup/v2ray-install.sh -y && cp ./config-backup/config.json /etc/v2ray/
 
 # 启动v2ray
-&& service v2ray start && echo /etc/v2ray/config.json && echo -e 'v2ray启动成功'
+service v2ray start && echo /etc/v2ray/config.json && echo -e 'v2ray启动成功'
 
 # 启用防火墙开放22端口,80端口,443端口
 ufw enable -y && ufw allow 22 && ufw allow 80 && ufw allow 443
