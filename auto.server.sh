15519a3ce6fd5bec4d9ed21019b0c147bcf91d55
yong <den9yon9@gmail.com>
2020-04-13 13:56:22 +0800

    fix: auto.sh

--- auto.server.sh
+++ auto.server.sh
@@ -1,17 +1,11 @@
-# 下载配置仓库
-git clone https://github.com/den9yon9/config-backup.git
-
 # 配置git
-cp ./config-backup/.gitconfig .
+cp ./config-backup/.gitconfig . && echo "git配置成功"
 
 # 安装并配置v2ray
 chmod a+x ./config-backup/v2ray-install.sh && ./config-backup/v2ray-install.sh -y && cp ./config-backup/config.json /etc/v2ray/
 
 # 启动v2ray
-service v2ray start && echo /etc/v2ray/config.json && echo -e 'v2ray启动成功'
-
-# 启用防火墙开放22端口,80端口,443端口
-ufw enable -y && ufw allow 22 && ufw allow 80 && ufw allow 443
+service v2ray start && cat /etc/v2ray/config.json && echo -e 'v2ray启动成功'
 
 # 安装nginx
 apt install nginx -y 
@@ -24,3 +18,6 @@ cp -r ./config-backup/cert /etc/nginx/
 
 # 重启nginx
 nginx -s reload
+
+# 防火墙开放22端口,80端口,443端口,并启用
+ufw allow 22 && ufw allow 80 && ufw allow 443  && echo y | ufw enable
