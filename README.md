3e5119cd15a00a5f3d50a096d9c7d1e57123a077
yong <den9yon9@gmail.com>
2020-04-16 15:07:21 +0800

    improvement: 合并auto脚本,更新README

--- README.md
+++ README.md
@@ -1,27 +1,6 @@
-1.设置Linux默认编辑器：update-alternatives --config editor
+1.运行自动配置脚本auto.sh
 2.创建用户adduser yong设置密码
 3.添加用户到sudo组: adduser yong sudo 
-4.登录yong生成用户密钥 ssh-keygen
-5.sudo visudo 编辑sudo组用户无需输入密码执行sudu操作 %sudo ALL=(ALL:ALL) NOPASSWD:ALL
-6.更换apt源
-    1) cd /etc/apt/ && mv ./sources.list sources.list.backup (备份源文件)
-    2) cp ~/config-backup/sources.list /etc/apt (设置新的源服务器)
-    3) sudo apt update 更新源
-
-7 安装nginx; sudo apt install nginx
-8 安装mysql; sudo apt install mysql-server
-9 启动MySQL服务 sudo service mysql start,注释掉配置文件中的bind-address: 127.0.0.1允许远程主机访问mysql服务
-10 安装nodejs:  sudo apt install make && curl -L https://git.io/n-install | bash && source .bashrc
-
-## 配置文件说明
-- default.conf (nginx配置)
-- n-install.sh (node版本安装脚本)
-- post-receive (git仓库自动部署钩子脚本)
-- source-list (apt源仓库)
-- ssr.sh (shadowsocksR安装脚本)
-- profile.json (windows terminal配置文件)
-- commitizen.sh (全局安转git提交格式规范插件commitizen)
-- CapsLock2Esc.reg (windows改键注册表，双击重启系统即可互换 CapsLock与 Esc键)
-- v2ray-install.sh (v2ray官网安装脚本)
-- .gitconfig (git配置文件)
-- config.json (v2ray配置文件)
+4.sudo visudo 编辑sudo组用户无需输入密码执行sudo操作 %sudo ALL=(ALL:ALL) NOPASSWD:ALL
+5.安装mysql, sudo apt install mysql-server
+6.启动mysql服务 sudo service mysql start,注释掉配置文件中的bind-address: 127.0.0.1允许远程主机访问mysql服务
---
+++ auto.sh
@@ -0,0 +1,50 @@
+# 备份源文件
+mv /etc/apt/sources.list sources.list.backup
+# 设置国内源
+cp ~/config-backup/apt/sources.list /etc/apt/
+# 更新源
+sudo apt update 
+
+# 设置Linux默认编辑器
+echo 3 | update-alternatives --config editor
+
+# 配置git
+cp ./config-backup/git/.gitconfig ~ && echo "git配置成功"
+
+# 安装make
+apt install make
+
+# 安装nodejs
+echo y | ./config-backup/nodejs/n-install.sh && source ~/.bashrc && echo nodejs安装成功
+# 安装commitizen
+./config-backup/nodejs/commitizen.sh
+# 安装http-server
+./config-backup/nodejs/http-server.sh
+
+# 安装nginx
+apt install nginx -y 
+# 存放证书到nginx配置目录下
+cp -r ./config-backup/nginx/cert /etc/nginx/
+# 配置nginx
+cp ./config-backup/nginx/conf.d/* /etc/nginx/conf.d/
+# 重启nginx
+nginx -s reload
+
+# wsl
+if [ -d /mnt/c ]; then
+    echo 'alias cdr="cd /mnt/c/Users/yong/repositories"' >> ~/.bashrc
+    echo 'alias cdd="cd /mnt/c/Users/yong/Desktop"' >> ~/.bashrc
+    echo 'alias o=explorer.exe' >> ~/.bashrc
+# 非wsl
+else
+    # 安装v2ray
+    ./config-backup/v2ray/v2ray-install.sh -y 
+    # 配置v2ray
+    cp ./config-backup/v2ray/config.json /etc/v2ray/config.json
+    # 启动v2ray
+    service v2ray start && cat /etc/v2ray/config.json && echo -e '\nv2ray启动成功'
+    # 防火墙开放22端口,80端口,443端口,并启用
+    ufw allow 22 && ufw allow 80 && ufw allow 443  && echo y | ufw enable
+    # 安装加速器
+    wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && ./tcp.sh
+fi
