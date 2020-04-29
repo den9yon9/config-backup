b3c5aa27a9c688df7af369048ec88cdaaf01269e
yong <den9yon9@gmail.com>
2020-04-29 06:54:07 +0000

    update auto.sh

--- auto.sh
+++ auto.sh
@@ -49,5 +49,5 @@ else
     # 防火墙开放22端口,80端口,443端口,并启用
     ufw allow 22 && ufw allow 80 && ufw allow 443  && echo y | ufw enable
     # 安装加速器
-    wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && ./tcp.sh
+    wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && chmod a+x ./tcp.sh && echo 2 | ./tcp.sh
 fi
