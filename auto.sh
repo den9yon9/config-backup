4b74509d7982c75ba4802661d8846adc27c2a70d
yong <den9yon9@gmail.com>
2020-07-04 00:53:08 +0800

    feat: 自动化脚本增加v2ray管理服务

--- auto.sh
+++ auto.sh
@@ -52,6 +52,9 @@ else
     service v2ray start && cat /etc/v2ray/config.json && echo -e '\nv2ray启动成功'
     # 防火墙开放22端口,80端口,443端口,并启用
     ufw allow 22 && ufw allow 80 && ufw allow 443  && echo y | ufw enable
+    # 安装v2ray管理服务
+    git clone https://github.com/den9yon9/v2ray-admin.git
+    cd v2ray && npm install && npm start
     # 安装加速器
     wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && chmod a+x ./tcp.sh && ./tcp.sh
 fi
