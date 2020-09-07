cbc7e243b13d7f80502b1889e835b3342d2e5149
yong <den9yon9@gmail.com>
2020-09-07 08:45:15 +0800

    fix:修复v2ray安装错误

--- v2ray/v2ray-install.sh
+++ v2ray/v2ray-install.sh
@@ -1,2 +1,2 @@
 # bash <(curl -L -s https://install.direct/go.sh)
-bash < (curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
+bash <(curl -L -s  https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
